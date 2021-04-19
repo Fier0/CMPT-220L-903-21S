4101a0810dd02ccc32f85b4c4d52341f8e80fd80
Alex Hanys <alexander.hanys1@marist.edu>
2021-04-19 18:47:13 -0400

    Assignment 6

---
+++ Assignments/Assignment 6/assgn6/src/Plant.java
@@ -0,0 +1,8 @@
+public class Plant{
+    public Plant(boolean needsWatering, String plantFamily, String name){
+        System.out.println("Name: "+name+" Family: "+ plantFamily + " Needs watering? "+ needsWatering);
+    }
+    public String wateringInstructions(){
+    return "Test";
+    }
+}
