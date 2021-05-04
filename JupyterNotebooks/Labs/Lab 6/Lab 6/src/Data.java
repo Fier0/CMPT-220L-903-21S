76c67267f70bc4c8b01ee3de04c574ea3563b998
Alex Hanys <alexander.hanys1@marist.edu>
2021-05-03 21:21:49 -0400

    lab 6
    Late because I was very confused on what was wanted

---
+++ JupyterNotebooks/Labs/Lab 6/Lab 6/src/Data.java
@@ -0,0 +1,12 @@
+
+public class Data {
+    int value;
+    Data nextData;
+
+    public Data(int value){
+        this.value = value;
+        this.nextData = null;
+    }
+
+
+}
--- JupyterNotebooks/Labs/Lab 6/Lab 6/src/lab_6.java
+++ JupyterNotebooks/Labs/Lab 6/Lab 6/src/lab_6.java
@@ -1,5 +1,14 @@
 public class lab_6 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+	    Data first = new Data(1);
+        Data second = new Data(2);
+        Data third = new Data(3);
+        Data fourth = new Data(4);
+        Data fifth = new Data(5);
+
+        first.nextData = second;
+        second.nextData = third;
+        third.nextData = fourth;
+        fourth.nextData = fifth;
     }
 }
